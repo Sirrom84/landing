bcf91f8979079cb826c339ebad02035653b6c1fa
Rob  Morris <morrisrjc@gmail.com>
2021-05-11 21:49:25 -0600

    adds additional styling

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import Head from "next/head";
 export default function Home() {
 	return (
 		<>
+			<Head>
+				<title>My page title</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
 			<h1 className="title"> Why Sphere?</h1>
-			{/* <div className="header-box">Take Advantage</div> */}
 			<div className="container">
 				<div className="tested">
 					{" "}
@@ -13,29 +16,15 @@ export default function Home() {
 						past. Sphere brings the world of coaching/guidance into the pockets
 						and homes of everyday people. For too long the idea of wellness and
 						self-improvement has felt like a luxury not afforded to the average
-						person. Sphere was created as a way to bring ANYONE to their full
-						potential by offering personalized coaching based on each person’s
-						individual needs. Whether it’s being a better parent, business
-						owner, leader, partner or friend Sphere can take down the walled
-						gardens of self-care and improvement.
+						person. Sphere was created as a way to bring{" "}
+						<strong> ANYONE </strong>to their full potential by offering
+						personalized coaching based on each person’s individual needs.
+						Whether it’s being a better parent, business owner, leader, partner
+						or friend Sphere can take down the walled gardens of self-care and
+						improvement.
 					</p>
 				</div>
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
-					voluptatibus pariatur nisi maiores nobis cumque ipsa nam optio
-					repellat dolorem. Esse neque, incidunt debitis quod exercitationem
-					magnam sed soluta architecto dolores beatae ut ipsa, voluptatem odio?
-					Debitis delectus sit ratione et fuga in. Saepe aliquid praesentium
-					earum sed voluptatibus repudiandae veniam excepturi voluptate
-					accusantium, quisquam animi omnis, adipisci amet ab reprehenderit!
-					Beatae libero nisi dicta molestiae mollitia perspiciatis dolor
-					corrupti, fugit voluptatum quia perferendis velit ratione ut cumque
-					provident deleniti id voluptatem, sint unde voluptas nobis quis
-					aspernatur ipsa labore? Illo, esse ea?
-				</p>
-				<div className="header-box">And That's OK</div>
-
+				<div className="banner">And That's OK</div>
 				<p>
 					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
 					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
@@ -65,7 +54,7 @@ export default function Home() {
 					provident deleniti id voluptatem, sint unde voluptas nobis quis
 					aspernatur ipsa labore? Illo, esse ea?
 				</p>
-				<div className="header-box">Become Free</div>
+				<div className="banner">Become Free</div>
 				<div className="why">
 					<img src="/images/BwAstro.png" alt="astro" />
 				</div>
@@ -99,10 +88,8 @@ export default function Home() {
 				</p>
 				<div className="accept">
 					<h1>
-						Why not take your potential<br></br> to new heights?
-						<br />
-						<br />
-						<span>Sphere</span>
+						Take your potential<br></br> to new heights?
+						<br /> Why NOT <a href="https://sphere.guide/"> Sphere?</a>
 					</h1>
 				</div>
 				<p>
