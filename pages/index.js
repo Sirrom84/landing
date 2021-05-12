3762a53e47fe6b4acddd5306d2b87851ee272a2e
Rob  Morris <morrisrjc@gmail.com>
2021-05-11 20:12:02 -0600

    adds images and parallax scrolling

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,138 @@ import Head from "next/head";
 
 export default function Home() {
 	return (
-		<div className="container">
-			<h1>Who We Are</h1>
-		</div>
+		<>
+			<h1 className="title"> Why Sphere?</h1>
+			{/* <div className="header-box">Take Advantage</div> */}
+			<div className="container">
+				<div className="tested">
+					{" "}
+					<p>
+						Going at it alone or “figuring it out” yourself is a thing of the
+						past. Sphere brings the world of coaching/guidance into the pockets
+						and homes of everyday people. For too long the idea of wellness and
+						self-improvement has felt like a luxury not afforded to the average
+						person. Sphere was created as a way to bring ANYONE to their full
+						potential by offering personalized coaching based on each person’s
+						individual needs. Whether it’s being a better parent, business
+						owner, leader, partner or friend Sphere can take down the walled
+						gardens of self-care and improvement.
+					</p>
+				</div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
+					voluptatibus pariatur nisi maiores nobis cumque ipsa nam optio
+					repellat dolorem. Esse neque, incidunt debitis quod exercitationem
+					magnam sed soluta architecto dolores beatae ut ipsa, voluptatem odio?
+					Debitis delectus sit ratione et fuga in. Saepe aliquid praesentium
+					earum sed voluptatibus repudiandae veniam excepturi voluptate
+					accusantium, quisquam animi omnis, adipisci amet ab reprehenderit!
+					Beatae libero nisi dicta molestiae mollitia perspiciatis dolor
+					corrupti, fugit voluptatum quia perferendis velit ratione ut cumque
+					provident deleniti id voluptatem, sint unde voluptas nobis quis
+					aspernatur ipsa labore? Illo, esse ea?
+				</p>
+				<div className="header-box">And That's OK</div>
+
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
+					voluptatibus pariatur nisi maiores nobis cumque ipsa nam optio
+					repellat dolorem. Esse neque, incidunt debitis quod exercitationem
+					magnam sed soluta architecto dolores beatae ut ipsa, voluptatem odio?
+					Debitis delectus sit ratione et fuga in. Saepe aliquid praesentium
+					earum sed voluptatibus repudiandae veniam excepturi voluptate
+					accusantium, quisquam animi omnis, adipisci amet ab reprehenderit!
+					Beatae libero nisi dicta molestiae mollitia perspiciatis dolor
+					corrupti, fugit voluptatum quia perferendis velit ratione ut cumque
+					provident deleniti id voluptatem, sint unde voluptas nobis quis
+					aspernatur ipsa labore? Illo, esse ea?
+				</p>
+
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
+					voluptatibus pariatur nisi maiores nobis cumque ipsa nam optio
+					repellat dolorem. Esse neque, incidunt debitis quod exercitationem
+					magnam sed soluta architecto dolores beatae ut ipsa, voluptatem odio?
+					Debitis delectus sit ratione et fuga in. Saepe aliquid praesentium
+					earum sed voluptatibus repudiandae veniam excepturi voluptate
+					accusantium, quisquam animi omnis, adipisci amet ab reprehenderit!
+					Beatae libero nisi dicta molestiae mollitia perspiciatis dolor
+					corrupti, fugit voluptatum quia perferendis velit ratione ut cumque
+					provident deleniti id voluptatem, sint unde voluptas nobis quis
+					aspernatur ipsa labore? Illo, esse ea?
+				</p>
+				<div className="header-box">Become Free</div>
+				<div className="why">
+					<img src="/images/BwAstro.png" alt="astro" />
+				</div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
+					voluptatibus pariatur nisi maiores nobis cumque ipsa nam optio
+					repellat dolorem. Esse neque, incidunt debitis quod exercitationem
+					magnam sed soluta architecto dolores beatae ut ipsa, voluptatem odio?
+					Debitis delectus sit ratione et fuga in. Saepe aliquid praesentium
+					earum sed voluptatibus repudiandae veniam excepturi voluptate
+					accusantium, quisquam animi omnis, adipisci amet ab reprehenderit!
+					Beatae libero nisi dicta molestiae mollitia perspiciatis dolor
+					corrupti, fugit voluptatum quia perferendis velit ratione ut cumque
+					provident deleniti id voluptatem, sint unde voluptas nobis quis
+					aspernatur ipsa labore? Illo, esse ea?
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
+					voluptatibus pariatur nisi maiores nobis cumque ipsa nam optio
+					repellat dolorem. Esse neque, incidunt debitis quod exercitationem
+					magnam sed soluta architecto dolores beatae ut ipsa, voluptatem odio?
+					Debitis delectus sit ratione et fuga in. Saepe aliquid praesentium
+					earum sed voluptatibus repudiandae veniam excepturi voluptate
+					accusantium, quisquam animi omnis, adipisci amet ab reprehenderit!
+					Beatae libero nisi dicta molestiae mollitia perspiciatis dolor
+					corrupti, fugit voluptatum quia perferendis velit ratione ut cumque
+					provident deleniti id voluptatem, sint unde voluptas nobis quis
+					aspernatur ipsa labore? Illo, esse ea?
+				</p>
+				<div className="accept">
+					<h1>
+						Why not take your potential<br></br> to new heights?
+						<br />
+						<br />
+						<span>Sphere</span>
+					</h1>
+				</div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
+					voluptatibus pariatur nisi maiores nobis cumque ipsa nam optio
+					repellat dolorem. Esse neque, incidunt debitis quod exercitationem
+					magnam sed soluta architecto dolores beatae ut ipsa, voluptatem odio?
+					Debitis delectus sit ratione et fuga in. Saepe aliquid praesentium
+					earum sed voluptatibus repudiandae veniam excepturi voluptate
+					accusantium, quisquam animi omnis, adipisci amet ab reprehenderit!
+					Beatae libero nisi dicta molestiae mollitia perspiciatis dolor
+					corrupti, fugit voluptatum quia perferendis velit ratione ut cumque
+					provident deleniti id voluptatem, sint unde voluptas nobis quis
+					aspernatur ipsa labore? Illo, esse ea?
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Reprehenderit et inventore maiores quae expedita est? Reprehenderit
+					voluptatibus pariatur nisi maiores nobis cumque ipsa nam optio
+					repellat dolorem. Esse neque, incidunt debitis quod exercitationem
+					magnam sed soluta architecto dolores beatae ut ipsa, voluptatem odio?
+					Debitis delectus sit ratione et fuga in. Saepe aliquid praesentium
+					earum sed voluptatibus repudiandae veniam excepturi voluptate
+					accusantium, quisquam animi omnis, adipisci amet ab reprehenderit!
+					Beatae libero nisi dicta molestiae mollitia perspiciatis dolor
+					corrupti, fugit voluptatum quia perferendis velit ratione ut cumque
+					provident deleniti id voluptatem, sint unde voluptas nobis quis
+					aspernatur ipsa labore? Illo, esse ea?
+				</p>
+			</div>
+		</>
 	);
 }
